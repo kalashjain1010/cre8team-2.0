1cb0d1d3538c9086b5081e8edb7471fd54472140
kalash jain <kalashjain54@gmail.com>
2023-10-01 19:25:41 +0530

    adding link to mobile devices

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,25 +1,25 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { Input } from './ui/input';
-import { Search } from 'lucide-react';
-import { cn } from '@/lib/utils';
-import { useContext } from 'react';
-import { UserContext } from '@/hooks/userUser';
-import { Button } from './ui/button';
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Input } from "./ui/input";
+import { Search } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useContext } from "react";
+import { UserContext } from "@/hooks/userUser";
+import { Button } from "./ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { toast } from 'react-hot-toast';
-import { useRouter } from 'next/navigation';
-import useModal from '@/hooks/useModal';
+} from "@/components/ui/dropdown-menu";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import useModal from "@/hooks/useModal";
 
 const Navbar = () => {
   const router = useRouter();
@@ -31,16 +31,16 @@ const Navbar = () => {
     if (error) {
       toast.error(error.message);
     } else {
-      toast.success('Logged out');
+      toast.success("Logged out");
     }
   };
   const userData = useContext(UserContext);
   const path = usePathname();
   const onClickDashboard = () => {
     if (userData?.user) {
-      router.push('dashboard');
+      router.push("dashboard");
     } else {
-      setOpen('auth-modal');
+      setOpen("auth-modal");
     }
   };
 
@@ -58,24 +58,24 @@ const Navbar = () => {
 
   // Attach the scroll event listener when the component mounts
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
       // Clean up the event listener when the component unmounts
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
     <div
       className={`${
-        hasScrolled ? 'fixed top-0 left-0 right-0 z-50' : ''
+        hasScrolled ? "fixed top-0 left-0 right-0 z-50" : ""
       } bg-white border border-[#ECECEC] rounded-[20px] px-6 py-4 transition-all`}
     >
       <div className="flex flex-row items-center justify-between">
         <div className="flex flex-row items-center gap-x-10">
           <Image
-            onClick={() => router.push('/projects')}
-            src={'/logo.svg'}
+            onClick={() => router.push("/projects")}
+            src={"/logo.svg"}
             alt="logo"
             width={150}
             height={40}
@@ -86,13 +86,13 @@ const Navbar = () => {
             <Input
               className="focus:outline-none bg-none active:outline-none border-none p-0 "
               placeholder="search"
-              disabled={path === '/about'}
+              disabled={path === "/about"}
             />
           </div>
           <Link
             className={cn(
-              path === '/projects' && 'text-[#0671E0]',
-              'hidden md:block text-[20px] font-medium hover:opacity-80 transition'
+              path === "/projects" && "text-[#0671E0]",
+              "hidden md:block text-[20px] font-medium hover:opacity-80 transition"
             )}
             href="/projects"
           >
@@ -100,8 +100,8 @@ const Navbar = () => {
           </Link>
           <p
             className={cn(
-              path === '/dashboard' && 'text-[#0671E0]',
-              ' hidden md:block text-[20px]  font-medium cursor-pointer hover:opacity-80 transition'
+              path === "/dashboard" && "text-[#0671E0]",
+              " hidden md:block text-[20px]  font-medium cursor-pointer hover:opacity-80 transition"
             )}
             onClick={onClickDashboard}
           >
@@ -109,8 +109,8 @@ const Navbar = () => {
           </p>
           <Link
             className={cn(
-              path === '/about' && 'text-[#0671E0]',
-              ' hidden md:block text-[20px]  font-medium hover:opacity-80 transition'
+              path === "/about" && "text-[#0671E0]",
+              " hidden md:block text-[20px]  font-medium hover:opacity-80 transition"
             )}
             href="/about"
           >
@@ -122,7 +122,7 @@ const Navbar = () => {
             <DropdownMenuTrigger className="z-10" asChild>
               <div className="rounded-full w-[50px] h-[50px] hover:cursor-pointer relative">
                 <Image
-                  src={userData.user.imageUrl || '/public/user.png'}
+                  src={userData.user.imageUrl || "/public/user.png"}
                   alt="avatar"
                   fill
                   className="rounded-full object-cover"
@@ -130,20 +130,34 @@ const Navbar = () => {
               </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent side="bottom" className="space-y-2 p-2">
-              <DropdownMenuItem className="cursor-pointer">Profile</DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer">
+                Profile
+              </DropdownMenuItem>
               <DropdownMenuSeparator className="md:hidden" />
-              <DropdownMenuItem className="md:hidden cursor-pointer">Project</DropdownMenuItem>
-              <DropdownMenuItem className="md:hidden cursor-pointer">Dashboard</DropdownMenuItem>
-              <DropdownMenuItem className="md:hidden cursor-pointer">About</DropdownMenuItem>
+              <DropdownMenuItem className="md:hidden cursor-pointer">
+                <Link href={"/projects"}>Project</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="md:hidden cursor-pointer">
+                <Link href={"/dashboard"}>Dashboard</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="md:hidden cursor-pointer">
+                <Link href={"/about"}>About</Link>
+              </DropdownMenuItem>
               <DropdownMenuSeparator className="md:hidden" />
-              <DropdownMenuItem onClick={() => SignOut()} className="cursor-pointer">
+              <DropdownMenuItem
+                onClick={() => SignOut()}
+                className="cursor-pointer"
+              >
                 <p className="text-rose-600">Logout</p>
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         ) : (
           <div>
-            <Button onClick={() => setOpen('auth-modal')} className="bg-[#0671E0] text-white">
+            <Button
+              onClick={() => setOpen("auth-modal")}
+              className="bg-[#0671E0] text-white"
+            >
               Login
             </Button>
           </div>
